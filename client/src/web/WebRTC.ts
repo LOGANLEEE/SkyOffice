4f9c838bc81f2e4ba8b2d95fcd6ca64d09d69b77
LOGANLEEE <logan_lee@kakao.com>
2021-12-19 22:23:04 +0000

    test

--- client/src/web/WebRTC.ts
+++ client/src/web/WebRTC.ts
@@ -126,12 +126,12 @@ export default class WebRTC {
     video.srcObject = stream
 
     video?.addEventListener('loadedmetadata', () => {
-      // if (!isSafari && !isMobile) {
-      video.play().then(() => {
-        if (this.videoGrid) this.videoGrid.append(video)
-      })
-      // }
+      video
+        .play()
+        .then((e) => console.debug('addVideoStream success:', e))
+        .catch((e) => console.debug('addVideoStream error:', e))
     })
+    if (this.videoGrid) this.videoGrid.append(video)
   }
 
   // method to remove video stream (when we are the host of the call)
